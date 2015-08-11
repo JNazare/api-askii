f64dc6ffbde9076b4e39e8e0628c380e11d86b41
jnazare <jules.nazare@gmail.com>
2015-08-11 14:10:51 -0400

    adding users

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ questions = [
         'answer': u'Milk, Cheese, Pizza, Fruit, Tylenol',
         'hint': u'Hinting',
         'done': False,
-        'courseId': 'test',
+        'courseId': u'test',
         'initialOrdering': 0.0
     },
     {
@@ -45,8 +45,18 @@ questions = [
         'answer': u'Need to find a good Python tutorial on the web',
         'hint': u'Hinting',
         'done': False,
-        'courseId': 'test',
+        'courseId': u'test',
         'initialOrdering': 1.0
+    }
+]
+
+users = [
+    {
+        'id': ObjectId(),
+        'personalData': {
+            "email": "test@test.com"
+        },
+        'answeredQuestions': {}
     }
 ]
 
@@ -60,23 +70,51 @@ question_fields = {
     'uri': fields.Url('question')
 }
 
+personal_data_fields = {}
+personal_data_fields['email']=fields.String
+personal_data_fields['name']=fields.String
+
+answered_questions_fields = {}
+
+user_fields = {
+    'personalData': fields.Nested(personal_data_fields),
+    'answeredQuestions': fields.Nested(answered_questions_fields)
+}
+
+
+class UserListAPI(Resource):
+    decorators = [auth.login_required]
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('answeredQuestions', type=dict, required=True, default={}, location='json')
+        self.reqparse.add_argument('personalData', type=dict, required=True, default={}, location='json')
+        super(UserListAPI, self).__init__()
+
+    def get(self):
+        return {'users': [marshal(user, user_fields) for user in users]}
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        user = {
+            'id': ObjectId(),
+            'personalData': args['personalData'],
+            'answeredQuestions': args['answeredQuestions']
+        }
+        users.appends(user)
+        return {'users': marshal(user, user_fields)}, 201
+
 
 class QuestionListAPI(Resource):
     decorators = [auth.login_required]
 
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('prompt', type=str, required=True,
-                                   help='No question prompt provided',
-                                   location='json')
-        self.reqparse.add_argument('answer', type=str, default="",
-                                   location='json')
-        self.reqparse.add_argument('hint', type=str, default="",
-                                   location='json')
-        self.reqparse.add_argument('initialOrdering', type=float, default=0.0,
-                                   location='json')
-        self.reqparse.add_argument('courseId', type=str, default="",
-                                   location='json')
+        self.reqparse.add_argument('prompt', type=str, required=True, help='No question prompt provided', location='json')
+        self.reqparse.add_argument('answer', type=str, default="", location='json')
+        self.reqparse.add_argument('hint', type=str, default="", location='json')
+        self.reqparse.add_argument('initialOrdering', type=float, default=0.0, location='json')
+        self.reqparse.add_argument('courseId', type=str, default="", location='json')
         super(QuestionListAPI, self).__init__()
 
     def get(self, courseId):
@@ -96,6 +134,42 @@ class QuestionListAPI(Resource):
         questions.append(question)
         return {'questions': marshal(question, question_fields)}, 201
 
+
+class UserAPI(Resource):
+    decorators = [auth.login_required]
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('answeredQuestions', type=dict, required=True, default={}, location='json')
+        self.reqparse.add_argument('personalData', type=dict, default={}, location='json')
+        super(UserAPI, self).__init__()
+
+    def get(self, id):
+        id = ObjectId(id)
+        user = [user for user in users if user['id'] == id]
+        if len(user) == 0:
+            abort(404)
+        return {'user': marshal(user[0], user_fields)}
+
+    def put(self, id):
+        id = ObjectId(id)
+        user = [user for user in users if user['id'] == id]
+        if len(user) == 0:
+            abort(404)
+        user = user[0]
+        args = self.reqparse.parse_args()
+        for k, v in args.items():
+            if v is not None:
+                user[k] = v
+        return {'user': marshal(user, user_fields)}
+
+    def delete(self, id):
+        id = ObjectId(id)
+        user = [user for user in users if user['id'] == id]
+        if len(user) == 0:
+            abort(404)
+        users.remove(user[0])
+        return {'result': True}
 
 class QuestionAPI(Resource):
     decorators = [auth.login_required]
@@ -138,6 +212,8 @@ class QuestionAPI(Resource):
 
 api.add_resource(QuestionListAPI, '/api/courses/<courseId>/questions', endpoint='questions')
 api.add_resource(QuestionAPI, '/api/courses/<courseId>/questions/<id>', endpoint='question')
+api.add_resource(UserListAPI, '/api/users', endpoint='users')
+api.add_resource(UserAPI, '/api/users/<id>', endpoint='user')
 
 
 if __name__ == '__main__':
